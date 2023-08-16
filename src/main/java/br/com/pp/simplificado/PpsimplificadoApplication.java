64d6313a11c7474a27d676f67f153b8b2231bab6
mjguedes <mjguedes@chesf.gov.br>
2023-08-15 23:05:04 -0300

    Task backlog

--- src/main/java/br/com/pp/simplificado/PpsimplificadoApplication.java
+++ src/main/java/br/com/pp/simplificado/PpsimplificadoApplication.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PpsimplificadoApplication {
 	public static void main(String[] args) {
 		/**
-		 * TODO habilitar spring security para permitir autenticação e geracao de tokens
+		 * TODO Tarefas
+		 * - Habilitar spring security para permitir autenticação e geração de tokens
+		 * - RuntimeExceptions são melhores do que Exceptions para tratar exceções de negócio
+		 * - Quando o desafio fala de transactions, provavelmente está falando de transações de banco de dados também
+		 * - Retornar statuscode 500 é errado, geralmente retornamos ou 2xx = sucesso, ou 4xx = erro
+		 * - Os serviços mockados seriam melhores aproveitados criando uma interface e uma classe que implementa
+		 * - Usar FeignClient no lugar do RestTemplate
+		 * - Docker para o banco de dados
+		 * - Usar Notification Pattern ao inves de lançar exceções?
+		 * - Usar um ci/cd com actions ou travis?
 		 */
 		SpringApplication.run(PpsimplificadoApplication.class, args);
 	}
